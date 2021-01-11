e8dea5e01d51d044554e506355d2529d6c06706c
Jul-Me <jmdev.uk@gmail.com>
2021-01-11 15:40:07 +0000

    Added missing functionality and finished the project.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const goBtn = document.getElementById('goBtn');
 const pages = document.querySelectorAll('.pages');
 const chooseDoggieBtn = document.querySelectorAll('.chooseDoggieBtn');
-
+const scoreElement = document.getElementById('score');
 const gameContainer = document.querySelector('.gameContainer');
 const time = document.getElementById('time');
+const annoyingMsg = document.getElementById('annoyingMsg');
 let selectedDoggie = {};
 let seconds = 0;
+let score = 0;
 
 goBtn.addEventListener('click', () => {
   pages[0].classList.add('up');
@@ -28,6 +30,21 @@ chooseDoggieBtn.forEach((btn) => {
   });
 });
 
+function increaseTime() {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  time.innerHTML = `Time: ${m}:${s}`;
+
+  seconds++;
+}
+
+function addDoggies() {
+  setTimeout(createDoggie, 1000);
+  setTimeout(createDoggie, 1500);
+}
+
 const createDoggie = () => {
   const doggie = document.createElement('div');
   const { x, y } = getRandomLocation();
@@ -40,9 +57,32 @@ const createDoggie = () => {
     selectedDoggie.alt
   }" style="transform: rotate(${Math.random() * 360}deg); width:100px"/>`;
 
-  // doggie.addEventListener('click', catchdoggie);
+  doggie.addEventListener('click', catchDoggie);
 
   gameContainer.appendChild(doggie);
+};
+
+function catchDoggie() {
+  increaseScore();
+  this.classList.add('catched');
+  setTimeout(() => {
+    this.remove();
+  }, 2000);
+  addDoggies();
+}
+
+function increaseScore() {
+  score++;
+
+  if (score > 19) {
+    annoyingMsg.classList.add('visible');
+  }
+
+  scoreElement.innerHTML = `Score: ${score}`;
+}
+
+const startGame = () => {
+  setInterval(increaseTime, 1000);
 };
 
 function getRandomLocation() {
@@ -52,18 +92,4 @@ function getRandomLocation() {
   const y = Math.random() * (height - 200) + 100;
 
   return { x, y };
-}
-
-const startGame = () => {
-  setInterval(increaseTime, 1000);
-};
-
-function increaseTime() {
-  let m = Math.floor(seconds / 60);
-  let s = seconds % 60;
-  m = m < 10 ? `0${m}` : m;
-  s = s < 10 ? `0${s}` : s;
-  time.innerHTML = `Time: ${m}:${s}`;
-
-  seconds++;
 }
